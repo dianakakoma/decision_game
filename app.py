0f5fccbc3506d8701558d412bc6cde96b21d22f7
dianakakoma <dikakoma@gmail.com>
2020-11-19 13:02:21 -0600

    all paths have a graceful termination point.

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ print("Welcome to my first game!")
 name = input("What is your name? ")
 age = int(input("What is your age? "))
 
-print("Hello", name,". You are", age, "years old!" )
-
-
 points = 10
-if age >= 18:
-    print("Hello", name,". At age", age, "you are old enough to play.")
+if age >= 14:
+    print("Hello",name,". At age", age, "you are old enough to play.")
 
     wants_to_play = input("Do you want to play? ").lower()
     if wants_to_play == "yes":
@@ -21,9 +18,9 @@ if age >= 18:
             ans = input("You follow a path and reach a lake. Do you swim across or go around? (swim/around) ")
 
             if ans == "swim":
-                print("Whoops! You got bit by a stingray and lost 5 points.")
-                points -= 5
-                print("You have ",points, "points.")
+                print("Whoops! You got a fatal bite from a stingray.")
+                points = 0
+                print("Game over. You have ",points, "points.")
             elif ans == "around":
                 points += 5
                 print("By going around, you now have", points, "points.")
@@ -34,19 +31,21 @@ if age >= 18:
                     points -= 5
                     print("The owner does not like you. You have lost 5 points.") 
                     print("You have", points, "points.")
-                    ans3 = input("Do you go into the forest to towards town? (forest/town)")
+                    ans3 = input("Do you go into the forest or towards town? (forest/town)")
 
                     if ans3 == "forest":
                         points = 0
                         print("Game Over,", name, ".You were eaten by a bear! You have zero points.")
                 #selected town
-                else:
-                    points += 5
-                    print("You made the right choice. With", points,"points, you win!")
+                    else:
+                        points += 5
+                        print("You made the right choice. With", points,"points, you win!")
 
-            else:
-                print("You lose!")
-                points = 0
+                else:
+                    
+                    points += 5
+                    print("You now have", points, "points.")
+                    print("Game over - you win!")
 
 
 #you chose right
@@ -54,10 +53,8 @@ if age >= 18:
             print("You tripped, fell and lost!")
             points = 0
     else:
-        print(" you have lost your 10 points. Buh bye!")
+        print("Buh bye,", name)
 
-elif age >= 14:
-    print("You can play as long as you have supervision.")
 else:
     print("Sorry,", name, "enjoy your youth some other way!")
 
